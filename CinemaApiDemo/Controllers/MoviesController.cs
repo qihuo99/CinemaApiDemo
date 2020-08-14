d1402f55bacc2740e0a2b69625b69244806978a5
juchu168 <qihuo99@users.noreply.github.com>
2020-08-14 15:24:54 +0800

    Create new Movie controller and model, code-first basis, runs fine.

---
+++ CinemaApiDemo/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaApiDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private List<Movie> movies = new List<Movie>
+        {
+            new Movie(){ Id=1, Name="Mission Impossible 7",  Language="English"},
+            new Movie(){ Id=2, Name="The Matrix 4",  Language="English"},
+            new Movie(){ Id=3, Name="Patriot Game",  Language="English"}
+        };
+
+        [HttpGet]  //Once the web request is send, the httpget will execute the Get() method
+        public IEnumerable<Movie> Get() 
+        {
+            return movies;
+        }
+
+
+    }
+}
